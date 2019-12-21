98b46a77a6cbbf215c30af5890718e92e1e2a2bd
Kdehner <Kdehner@gmail.com>
2019-12-21 13:36:40 -0700

    Updating Main

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
-const roleHarvester = require();
+var roleHarvester = require('role.harvester');
 
 module.exports.loop = function () {
-  const spawn1 = Game.spawns['Spawn1'];
 
-  for (let name in Memory.creeps) {
-    if (!Game.creeps[name]) {
-      delete Memory.creeps[name];
-      console.log(`Clearing non-existing creep memory:${name}`);
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
     }
-  }
 
-  const harvesters = _.filter(Game.creeps, creep => creep.memory.role === 'harvester');
-  
-  if(harvesters.length < 2) {
-    const newName = `Harvester${Game.time}`;
-    console.log(`Spawning new harvester: ${newName}`);
-    spawn1.spawnCreep([WORK,CARRY,MOVE], newName, {memory: {role: 'harvester'}});
-  }
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
 
-  if(spawn1.spawning) {
-    const spawnCreep = Game.creeps[spawn1.spawning.name];
-    spawn1.room.visual.text(
-      `🛠️ ${spawningCreep.memory.role}`,
-      spawn1.pos.x + 1,
-      spawn1.pos.y,
-      {align: 'left', opacity: 0.8}
-    );
-  }
-
-  for (let name in Game.creeps) {
-    const creep = Game.creeps[name];
-    if(creep.memory.role === 'harvester') {
-      roleHarvester.run(creep);
+    if(harvesters.length < 2) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+            {memory: {role: 'harvester'}});
     }
-  }
 
+    if(Game.spawns['Spawn1'].spawning) {
+        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1,
+            Game.spawns['Spawn1'].pos.y,
+            {align: 'left', opacity: 0.8});
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+    }
 }
